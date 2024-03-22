4b042d0edfde178c0ab68268311d3cb1745e9243
5udoku <mvys9abg2tfajwzd5r@bk.ru>
2024-03-22 16:02:52 +0200

    Add private routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import Login from './features/auth/Login'
-import Register from './features/auth/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PrivateRoutes from './components/shared/ProtectedRoutes';
+import Login from './features/auth/Login';
+import Register from './features/auth/Register';
+
 
 function App() {
   return (
-    <>
-      <Login />
-      <Register />
-    </>
-  )
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+
+          </Route>
+          <Route element={<Login />} path="/login" />
+          <Route element={<Register />} path="/register" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
